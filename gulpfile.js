01b1baa25674eb98a61fc64c40106501f6b5898d
AndyScorp <scorpionand28@gmail.com>
2018-01-17 10:11:57 +0200

    add new task

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,38 @@ const gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync'),
     pug = require('gulp-pug'),
-    concat = require('gulp-concat'),
     uglify = require('gulp-uglifyjs'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    htmlbeautify = require('gulp-html-beautify'),
+    cssbeautify = require('gulp-cssbeautify'),
+    babel = require('gulp-babel'),
+    minify = require('gulp-minify'),
+    webpack = require('webpack-stream'),
+    path = require('path');
 
 
 gulp.task('sass', function () {
     return gulp.src('src/sass/main.sass')
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssbeautify())
         .pipe(sourcemaps.write('./maps', { addComment: false }))
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('concat', function () {
-    return gulp.src(['src/sass/base/*.sass', 'src/sass/layout/*.sass', 'src/sass/module/*.sass', 'src/sass/state/*.sass'])
-        .pipe(concat('main.sass'))
-        .pipe(gulp.dest('src/sass'))
-});
-
 gulp.task('views', function buildHTML() {
+    var options = { indentSize: 2 };
     return gulp.src('./src/pug/index.pug')
         .pipe(pug())
+        .pipe(htmlbeautify(options))
         .pipe(gulp.dest('src'))
         .pipe(browserSync.reload({ stream: true }));
 });
@@ -44,7 +51,15 @@ gulp.task('browser-sync', function () {
 // min js
 gulp.task('js-min', function () {
     return gulp.src('src/js/**/*.js')
-        // .pipe(uglify())
+        .pipe(babel())
+        .pipe(minify({
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['-min.js']
+        }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('docs/js'));
 });
@@ -77,7 +92,7 @@ gulp.task('bild-docs', ['html', 'cssmin', 'js-min', 'img']);
 
 
 gulp.task('watch', ['views', 'sass', 'browser-sync'], function () {
-    gulp.watch('src/sass/**/*.+(scss|sass)', ['concat', 'sass']);
+    gulp.watch('src/sass/**/*.+(scss|sass)', ['sass']);
     gulp.watch('src/pug/*.pug', ['views']);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload());
 });
